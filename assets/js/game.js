e0be04deb80c3ba326da6995e468d4d4f91f463a
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-01-21 16:36:13 -0700

    feature/start-quiz

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,54 @@
-var buttonEl = document.querySelector("#start-quiz")
-var optionsEl = document.querySelector("#pick-an-option")
+const StartBtn = document.querySelector(".start-btn")
+const title = document.querySelector(".title")
 
-var nextQuestion = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.textContent = "next question";
-    optionsEl.appendChild(listItemEl);
+function remove(el) {
+    var element = el;
+    element.remove();
 }
 
-buttonEl.addEventListener("click", nextQuestion);
+const displayQuestions = function() {
+    questions.innerHTML = "";
+    options.innerHTML = "";
+    quizContainer.appendChild(questions);
+}
+
+const questionArray = [
+{
+    question: 'The condition in an if/ else statement is enclosed with ____.',
+    options: [
+        'quotes',
+        'curly brackets',
+        'paranthesis',
+        'square brackets'
+    ],
+    answer: 'paranthesis'
+},
+
+{
+    question: 'Arrays in JavaScript can be used to store ____.',
+    options: [
+        'numbers and strings',
+        'other arrays',
+        'booleans',
+        'all of the above'
+    ],
+    answer: 'all of the above'
+},
+
+{
+    question: 'In how many ways a JavaScript code can be involved in an HTML file?',
+    options: [
+        'inline',
+        'external',
+        'internal',
+        'all of the above'
+    ],
+    answer: 'all of the above'
+}
+]
+
+console.log(questionArray);
+
+
+
+
